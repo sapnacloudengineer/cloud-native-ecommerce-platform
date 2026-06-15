b3a15e50b2b591778711420804619e25da25423d
Ubuntu <ubuntu@ip-172-31-0-196.ap-south-1.compute.internal>
2026-06-15 06:16:57 +0000

    Add k8s manifests and Jenkins updates

---
+++ terraform/main.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+  region = "ap-south-1"
+}
+
+resource "aws_instance" "devops_server" {
+  ami           = "ami-0f58b397bc5c1f2e8" # Amazon Linux 2023 (Mumbai)
+  instance_type = "t3.small"
+
+  tags = {
+    Name = "terraform-ec2"
+  }
+}
