630aae21b734645e499e115962450c460f0815f3
Ubuntu <ubuntu@ip-172-31-2-165.ap-south-1.compute.internal>
2026-06-30 07:50:23 +0000

    new update

---
+++ terraform/ec2.tf
@@ -0,0 +1,9 @@
+
+resource "aws_instance" "devops_server" {
+  ami           = "ami-0f58b397bc5c1f2e8" # Amazon Linux 2023 (Mumbai)
+  instance_type = "t3.small"
+
+  tags = {
+    Name = "terraform-ec2"
+  }
+}
